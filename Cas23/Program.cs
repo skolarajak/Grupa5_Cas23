50a896eb356955abf6b7b1c92faa4faa1707c2c2
Skola Rajak QA <skolarajak.qa@gmail.com>
2019-11-18 20:38:06 +0100

    Completed tasks 1 and 2

--- Cas23/Program.cs
+++ Cas23/Program.cs
@@ -10,9 +10,32 @@ namespace Cas23
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[] { 10, 5, 9, 2, 6 };
-            int max = FindLargestNum(numbers);
-            Console.WriteLine("Najveci u nizu je {0}", max);
+            /*
+             * Zadatak 1.
+             * 
+             * Pronađi najveći broj u nizu             * Napisati funkciju koja će kao ulazni parametar
+             * imati niz brojeva. Vratiti najveći broj iz niza.
+             */
+
+            int[] numbers1 = new int[] { 1, 5, 9, 2, 6 };
+            int max = FindLargestNum(numbers1);
+            Console.WriteLine("1. Najveci u nizu je {0}", max);
+
+            /*
+             * Zadatak 2.
+             * Razlika između najmanjeg i najvećeg broja
+             * 
+             * Napisati funkciju koja će kao ulazni parametar
+             * imati niz brojeva. Vratiti razliku između najvećeg
+             * i najmanjeg broja.
+             */
+
+            int[] numbers2 = new int[] { -20, 16, 48, 5, -15, 2, 1, 9, 16, -43 };
+            int diff = DiffMinMax(numbers2);
+            Console.WriteLine("2. Razlika max-min je {0}", diff);
+
+
+
             Console.ReadKey();
         }
 
@@ -29,5 +52,25 @@ namespace Cas23
 
             return max;
         }
+
+        static int DiffMinMax(int[] numbers)
+        {
+            int min = numbers[0];
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+                if (min > numbers[i])
+                {
+                    min = numbers[i];
+                }
+            }
+            int diff = max - min;
+            return diff;
+        }
+
     }
 }
